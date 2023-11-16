d064174156b908583ef2f49c1d877a63fe4b5d42
Jomar Banting <shadowlover.05@gmail.com>
2023-11-16 04:24:42 -0500

    fixed server.js router path directory

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
-const router = require('./router');
+const router = require('./recipes/recipes-router');
 
 const server = express();
 
